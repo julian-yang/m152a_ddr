c7fdb80ca4cf0d557285615f70b5d49aeb1b58f7
Julian Yang <yang.julian@gmail.com>
2014-12-03 23:29:53 -0800

    implements random.v (generate random bits using a linear feedback shift register)

--- random.v
+++ random.v
@@ -22,8 +22,24 @@ module random(
     //input 
     clk, sw,
     //output
-    random_num
+    random_num,
+    random_arrow
     );
 
+input clk;
+input [5:0] sw;
+output [5:0] random_num;
+output [4:0] random_arrow;
+
+reg [5:0] random_reg = sw;
+
+always @ (posedge clk)
+begin
+    newBit = random_num[5] + random_num[1] + 1;
+    random_num_reg = {6{newBit,random_num}};
+end
+
+assign random_num = random_num_reg;
+assign random_arrow = random_num_reg % 10;
 
 endmodule
